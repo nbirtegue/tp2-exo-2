6770326219c3c018a5d964d1461d2c9964193796
nbirtegue <68041767+nbirtegue@users.noreply.github.com>
2020-07-24 10:59:30 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     Player1 = Player
     Player += -1
+    led.unplot(Player1, 4)
     if (Player < 0) {
         Player = 0
     }
-    led.unplot(Player1, 4)
     led.plot(Player, 4)
 })
 input.onButtonPressed(Button.B, function () {
     Player1 = Player
     Player += 1
+    led.unplot(Player1, 4)
     if (Player > 4) {
         Player = 4
-        led.unplot(Player1, 4)
-        led.plot(Player, 4)
     }
+    led.plot(Player, 4)
 })
 let pointX = 0
 let Player1 = 0
@@ -22,13 +22,24 @@ let Player = 0
 Player = 2
 led.plot(Player, 4)
 let PointY = -1
+let Score = 0
 basic.forever(function () {
     led.unplot(pointX, PointY)
+    led.plot(Player, 4)
     if (PointY == 4) {
         pointX = randint(0, 4)
         PointY = -1
     }
     PointY += 1
     led.plot(pointX, PointY)
+    if (PointY == 4 && Player == pointX) {
+        Score += 1
+    } else if (PointY == 4 && Player != pointX) {
+        basic.clearScreen()
+        basic.showString("Score:")
+        basic.showNumber(Score)
+        basic.clearScreen()
+        Score = 0
+    }
     basic.pause(500)
 })
