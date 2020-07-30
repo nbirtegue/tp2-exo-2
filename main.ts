cb0d3e5c7dfb97e29e10291dc15e52b2797447d5
nbirtegue <68041767+nbirtegue@users.noreply.github.com>
2020-07-31 09:17:23 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,42 @@
 input.onButtonPressed(Button.A, function () {
-    Player1 = Player
-    Player += -1
-    led.unplot(Player1, 4)
-    if (Player < 0) {
-        Player = 0
+    led.unplot(player, 4)
+    player += -1
+    if (player < 0) {
+        player = 0
     }
-    led.plot(Player, 4)
+    led.plot(player, 4)
 })
 input.onButtonPressed(Button.B, function () {
-    Player1 = Player
-    Player += 1
-    led.unplot(Player1, 4)
-    if (Player > 4) {
-        Player = 4
+    led.unplot(player, 4)
+    player += 1
+    if (player > 4) {
+        player = 4
     }
-    led.plot(Player, 4)
+    led.plot(player, 4)
 })
-let Score = 0
-let pointX = 0
-let Player1 = 0
-let Player = 0
-Player = 2
-led.plot(Player, 4)
+let score = 0
+let player = 0
+player = 2
+led.plot(player, 4)
+let PointX = randint(0, 4)
 let PointY = -1
-let Score = 0
 basic.forever(function () {
-    led.unplot(pointX, PointY)
-    led.plot(Player, 4)
+    led.unplot(PointX, PointY)
+    led.plot(player, 4)
     if (PointY == 4) {
-        pointX = randint(0, 4)
+        PointX = randint(0, 4)
         PointY = -1
     }
     PointY += 1
-    led.plot(pointX, PointY)
-    if (PointY == 4 && Player == pointX) {
-        Score += 1
-    } else if (PointY == 4 && Player != pointX) {
-        basic.clearScreen()
-        basic.showString("Score:")
-        basic.showNumber(Score)
-        basic.clearScreen()
-        Score = 0
-    }
+    led.plot(PointX, PointY)
     basic.pause(500)
-    if (PointY == 4 && Player == pointX) {
-        Score += 1
-    } else if (PointY == 4 && Player != pointX) {
+    if (PointY >= 4 && player == PointX) {
+        score += 1
+    } else if (PointY >= 4 && player != PointX) {
         basic.clearScreen()
-        basic.showString("Score:")
-        basic.showNumber(Score)
+        basic.showString("score:")
+        basic.showNumber(score)
+        player = 2
+        basic.clearScreen()
     }
 })
